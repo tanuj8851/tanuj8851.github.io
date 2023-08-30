f07937a9d4630f25d7bf038a0b2a720b85de66ec
tanuj8851 <tanujmaurya40@gmail.com>
2023-08-30 14:20:31 +0530

    Project section done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // ------------Navigation Bar--------------
-let menuIcon=document.querySelector("#menu-icon");
-let navbar=document.querySelector(".navbar");
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick=()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x")
     navbar.classList.toggle("active")
 }
@@ -14,37 +14,37 @@ menuIcon.onclick=()=>{
 
 
 
-GitHubCalendar(".calendar", "mrunalibind");
+GitHubCalendar(".calendar", "tanuj8851");
 
-    // or enable responsive functionality:
-    GitHubCalendar(".calendar", "mrunalibind", { responsive: true });
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "tanuj8851", { responsive: true });
 
-    // Use a proxy
-    GitHubCalendar(".calendar", "mrunalibind", {
-       proxy (mrunalibind) {
-         return fetch(`https://your-proxy.com/github?user=${mrunalibind}`)
-       }
-    })
-    // .then(r => r.text())
+// Use a proxy
+GitHubCalendar(".calendar", "tanuj8851", {
+    proxy(mrunalibind) {
+        return fetch(`https://your-proxy.com/github?user=${tanuj8851}`)
+    }
+})
+// .then(r => r.text())
 
 
 
-    const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll("section[id]");
 
 // Add an event listener listening for scroll
 // window.addEventListener("scroll", navHighlighter);
 
 // function navHighlighter() {
-  
+
 //   // Get current scroll position
 //   let scrollY = window.pageYOffset;
-  
+
 //   // Now we loop through sections to get height, top and ID values for each
 //   sections.forEach(current => {
 //     const sectionHeight = current.offsetHeight;
 //     const sectionTop = current.offsetTop - 50;
 //     sectionId = current.getAttribute("id");
-    
+
 //     /*
 //     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
 //     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
@@ -66,12 +66,12 @@ GitHubCalendar(".calendar", "mrunalibind");
 
 // ----------scroll reveal--------------
 ScrollReveal({
-    reset:true,
-    distance:'80px',
-    duration:2000,
-    delay:200
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
 
 });
 
-ScrollReveal().reveal('.home-content, .heading', {origin:'top'})
-ScrollReveal().reveal('.home-img, .project-card, .skills-card', {origin:'bottom'})
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' })
+ScrollReveal().reveal('.home-img, .project-card, .skills-card', { origin: 'bottom' })
